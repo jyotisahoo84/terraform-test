d059a4a600b9f99afce5673e451720dbf9203252
jyotiranjansahoo <Jyoti.sahoo84@gmail.com>
2026-08-29 13:04:06 +0530

    Format Terraform files

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,30 +1,30 @@
 module "resg1" {
 
-    source = "../../child_module/resource_group"
-    rgs = var.rgs
-  
+  source = "../../child_module/resource_group"
+  rgs    = var.rgs
+
 }
 
 module "virnet1" {
 
-    depends_on = [ module.resg1 ]
-    source = "../../child_module/vnet"
-    vnets = var.vnets
-  
+  depends_on = [module.resg1]
+  source     = "../../child_module/vnet"
+  vnets      = var.vnets
+
 }
 
 module "subnet12" {
 
-    depends_on = [ module.virnet1 ]
-    source = "../../child_module/subnet"
-    snets = var.snets
-  
+  depends_on = [module.virnet1]
+  source     = "../../child_module/subnet"
+  snets      = var.snets
+
 }
 
 module "pip" {
 
-    depends_on = [ module.resg1 ]
-    source = "../../child_module/public_ip"
-    pip = var.pip
-  
+  depends_on = [module.resg1]
+  source     = "../../child_module/public_ip"
+  pip        = var.pip
+
 }
